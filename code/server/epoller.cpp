2ed821fc9a7dcfe87ce8a044a5b816c02ca70614
lemonQwQ <1282920248@qq.com>
2021-06-12 17:42:31 +0800

    update epoller

---
+++ code/server/epoller.cpp
@@ -0,0 +1,44 @@
+#include "epoller.h"
+
+Epoller::Epoller(int maxEvent):epollFd_(epoll_create(512)), events_(maxEvent) {
+  assert(epollFd_ >= 0 && events_.size() > 0);  
+}
+
+Epoller::~Epoller() {
+  close(epollFd_);    
+}
+
+bool Epoller::AddFd(int fd, uint32_t events) {
+  if (fd < 0) return false;
+  epoll_event event = {0};
+  event.data.fd = fd;
+  event.events = events;
+  return 0 == epoll_ctl(epollFd_, EPOLL_CTL_ADD, fd, &event);    
+} 
+
+bool Epoller::ModFd(int fd, uint32_t events) {
+  if (fd < 0) return false;
+  epoll_event event = {0};
+  event.data.fd = fd;
+  event.events = events;
+  return 0 == epoll_ctl(epollFd_, EPOLL_CTL_MOD, fd, &event);
+}
+
+bool Epoller::DelFd(int fd) {
+  if (fd < 0) return false;
+  return 0 == epoll_ctl(epollFd_, EPOLL_CTL_DEL, fd, nullptr);
+}
+
+int Epoller::Wait(int timeoutMs = -1) {
+  return epoll_wait(epollFd_, &events_[0], static_cast<int>(events_.size()), timeoutMs);
+}
+
+int Epoller::GetEventFd(size_t idx) const {
+  assert(idx >= 0 && idx < events_.size());
+  return events_[idx].data.fd;
+}
+
+uint32_t Epoller::GetEvents(size_t idx) const {
+  assert(idx >= 0 && idx < events_.size());
+  return events_[idx].events;
+}
--- code/server/epoller.h
+++ code/server/epoller.h
@@ -15,16 +15,17 @@ public:
 
   bool AddFd(int fd, uint32_t events); // 添加 监听事件类型events的文件描述符为fd 
   bool ModFd(int fd, uint32_t events); // 更改 监听事件类型events的文件描述符为fd
-  bool DelFd(int fd, uint32_t events); // 删除 监听事件类型events的文件描述符为fd
+  bool DelFd(int fd); // 删除 监听事件类型events的文件描述符为fd
   
   int Wait(int timeoutMs = -1); // 监听等待epoll事件发生
   
   int GetEventFd(size_t idx) const; // 获取索引值idx对应的文件描述符
 
-   uint32_t GetEvents(size_t idx) const; // 获取索引值idx的事件类型
+  uint32_t GetEvents(size_t idx) const; // 获取索引值idx的事件类型
 
 private:
-  int epollfd_; // epoller内核开辟的空间文件描述符
+
+  int epollFd_; // epoller内核开辟的空间文件描述符
   std::vector<struct epoll_event> events_;
 };
 
